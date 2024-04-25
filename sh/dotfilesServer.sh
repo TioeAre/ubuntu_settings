73daed081a1212c577b2b0000b69e52c80bbf230
tioeare <are10032@outlook.com>
2024-04-26 01:54:09 +0800

    update dotfiles

--- sh/dotfilesServer.sh
+++ sh/dotfilesServer.sh
@@ -13,6 +13,7 @@ ln -s "$parent_dir/dotFiles/.cmake-format.yaml" ~/
 ln -s "$parent_dir/dotFiles/.cpplint" ~/
 ln -s "$parent_dir/dotFiles/.flake8" ~/
 ln -s "$parent_dir/dotFiles/.tmux.conf" ~/
-ln -s "$parent_dir/dotFiles/.vale.ini" ~/
+ln -s "$parent_dir/dotFiles/.tmux.conf" ~/
+ln -s "$parent_dir/dotFiles/.tmux" ~/
 # ln -s "$parent_dir/dotFiles/.vimrc" ~/
 ln -s "$parent_dir/dotFiles/.config/tmux-powerline" ~/.config
