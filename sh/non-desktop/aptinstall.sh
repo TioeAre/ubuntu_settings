761b74246bf21e78caa218855d0b59c5c3c996b1
TioeAre <are10032@outlook.com>
2024-07-13 01:32:49 +0800

    update aptinstall.sh fuse for nvim

--- sh/non-desktop/aptinstall.sh
+++ sh/non-desktop/aptinstall.sh
@@ -4,4 +4,4 @@
 cd $(dirname "$0") || exit
 sh ./aptsource.sh
 
-sudo apt install git curl wget make gawk scdoc build-essential htop unzip -y
+sudo apt install git curl wget make gawk scdoc build-essential htop unzip fuse -y
