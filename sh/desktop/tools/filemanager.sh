73abe6876a05fd885c4d48d282012820a7bbe9d0
TioeAre <are10032@outlook.com>
2024-10-06 11:06:18 -0400

    Refactor alacritty.toml and filemanager.sh scripts

--- sh/desktop/tools/filemanager.sh
+++ sh/desktop/tools/filemanager.sh
@@ -3,3 +3,7 @@
 
 cd ~/tools || exit
 sudo apt install nautilus-actions filemanager-actions -y
+
+# https://github.com/bassmanitram/actions-for-nautilus
+# /usr/bin/x-terminal-emulator --working-directory=%d/%b
+# nautilus -q
