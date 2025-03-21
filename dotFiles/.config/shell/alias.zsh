598400e7fe0769d60b57df199edf24986f4a7f16
TioeAre <are10032@outlook.com>
2025-03-21 13:01:22 +0800

    add new aliases and update application configurations for Zed editor

--- dotFiles/.config/shell/alias.zsh
+++ dotFiles/.config/shell/alias.zsh
@@ -4,3 +4,5 @@
 
 alias socks5="http_proxy=http://127.0.0.1:7890 https_proxy=http://127.0.0.1:7890 all_proxy=http://127.0.0.1:7890 wss_proxy=http://127.0.0.1:7890"
 alias ross="source /opt/ros/jazzy/setup.zsh"
+alias nvimn="nvim -u NORC"
+alias vimn="nvim -u NORC"
