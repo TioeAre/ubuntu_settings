77174214290fe1d58787c90f366267736b3551ef
TioeAre <are10032@outlook.com>
2025-09-10 15:38:09 +0800

    update chmod cudnn

--- sh/non-desktop/packages/cuda.sh
+++ sh/non-desktop/packages/cuda.sh
@@ -6,11 +6,11 @@ sh cuda_12.6.0_560.28.03_linux.run
 wget https://developer.download.nvidia.com/compute/cudnn/redist/cudnn/linux-x86_64/cudnn-linux-x86_64-9.10.2.21_cuda12-archive.tar.xz
 tar -xvf cudnn-linux-x86_64-9.10.2.21_cuda12-archive.tar.xz
 
-cp include/cudnn*.h /home/lixuan/cuda-12.6/include/
-cp lib/libcudnn* /home/lixuan/cuda-12.6/lib64/
+cp include/cudnn*.h /home/lixuan/cuda12.6/include/
+cp lib/libcudnn* /home/lixuan/cuda12.6/lib64/
 
-sudo chmod a+r /home/lixuan/cuda-12.6/include/cudnn*.h /home/lixuan/cuda-12.6/lib64/libcudnn*
+chmod a+r /home/lixuan/cuda12.6/include/cudnn*.h /home/lixuan/cuda12.6/lib64/libcudnn*
 
-# export CUDA_HOME=/home/lixuan/cuda-12.6
-# export PATH=/home/lixuan/cuda-12.6/bin:$PATH
-# export LD_LIBRARY_PATH=/home/lixuan/cuda-12.6/lib64:$LD_LIBRARY_PATH
+# export CUDA_HOME=/home/lixuan/cuda12.6
+# export PATH=/home/lixuan/cuda12.6/bin:$PATH
+# export LD_LIBRARY_PATH=/home/lixuan/cuda12.6/lib64:$LD_LIBRARY_PATH
