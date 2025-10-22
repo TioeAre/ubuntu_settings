2a91562b8f76f07d1d0bfd02ba955e1beede29b1
TioeAre <are10032@outlook.com>
2025-10-22 18:16:14 +0800

    feat(zellij): improve local and public IP detection scripts

--- dotFiles/.config/zellij/command/local_ip.sh
+++ dotFiles/.config/zellij/command/local_ip.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/sh
 
-local_ip=$(hostname -I 2>/dev/null | awk '{print $1}')
+# local_ip=$(hostname -I 2>/dev/null | awk '{print $1}')
+# if [ -z "$local_ip" ]; then
+#     local_ip=$(ip addr show 2>/dev/null | grep 'inet ' | grep -v '127.0.0.1' | grep 'global' | awk '{print $2}' | cut -d'/' -f1 | head -n 1)
+# fi
+# echo "$local_ip"
 
-if [ -z "$local_ip" ]; then
-    local_ip=$(ip addr show 2>/dev/null | grep 'inet ' | grep -v '127.0.0.1' | grep 'global' | awk '{print $2}' | cut -d'/' -f1 | head -n 1)
+
+### reference https://github.com/erikw/tmux-powerline/blob/main/segments/lan_ip.sh
+lan_ip=""
+os_name=$(uname)
+if [ "$os_name" = "Linux" ]; then
+    lan_ip=$(ip route get 1.1.1.1 2>/dev/null | awk '{for(i=1;i<=NF;i++) if($i=="src") {print $(i+1); exit}}')
+    if [ -z "$lan_ip" ]; then
+        lan_ip=$(ip addr show 2>/dev/null | awk '/inet / && /global/ {sub(/\/.*$/, "", $2); print $2; exit}')
+    fi
+else
+    default_nic=$(route get default 2>/dev/null | grep -i 'interface:' | awk '{print $2}')
+    all_nics=$(ifconfig 2>/dev/null | awk -F':' '/^[a-z]/ && !/^lo/ { print $1 }')
+    nic_list=$(echo "$default_nic $all_nics" | tr ' ' '\n' | uniq | tr '\n' ' ')
+    for nic in $nic_list; do
+        lan_ip=$(ifconfig "$nic" 2>/dev/null | awk '$1 == "inet" && $2 != "127.0.0.1" { print $2; exit }')
+        if [ -n "$lan_ip" ]; then
+            break
+        fi
+    done
 fi
-
-echo "$local_ip"
+echo "${lan_ip:-N/A}"
--- dotFiles/.config/zellij/command/public_ip.sh
+++ dotFiles/.config/zellij/command/public_ip.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/sh
 
-if command -v curl >/dev/null 2>&1; then
-    public_ip=$(curl -s ifconfig.me)
-elif command -v wget >/dev/null 2>&1; then
-    public_ip=$(wget -qO- ifconfig.me)
-else
-    public_ip="None curl or wget installed"
-fi
+# if command -v curl >/dev/null 2>&1; then
+#     public_ip=$(curl -s ifconfig.me)
+# elif command -v wget >/dev/null 2>&1; then
+#     public_ip=$(wget -qO- ifconfig.me)
+# else
+#     public_ip="None curl or wget installed"
+# fi
+# echo "$public_ip"
 
-echo "$public_ip"
+
+### reference https://github.com/erikw/tmux-powerline/blob/main/segments/wan_ip.sh
+TIMEOUT=3
+wan_ip=""
+if command -v curl >/dev/null 2>&1; then
+    wan_ip=$(curl -s --max-time $TIMEOUT https://whatismyip.akamai.com/)
+    if [ -z "$wan_ip" ]; then
+        wan_ip=$(curl -s --max-time $TIMEOUT https://ifconfig.me/)
+    fi
+elif command -v wget >/dev/null 2>&1; then
+    wan_ip=$(wget -qO- --timeout=$TIMEOUT https://whatismyip.akamai.com/)
+    if [ -z "$wan_ip" ]; then
+        wan_ip=$(wget -qO- --timeout=$TIMEOUT https://ifconfig.me/)
+    fi
+fi
+echo "${wan_ip:-N/A}"
