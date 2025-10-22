39aead1735861584ab3ae2cab26d357eb4223b23
TioeAre <are10032@outlook.com>
2025-10-22 10:45:04 +0800

    update proxy

--- sh/non-desktop/packages/cuda.sh
+++ sh/non-desktop/packages/cuda.sh
@@ -2,10 +2,14 @@
 
 wget https://developer.download.nvidia.com/compute/cuda/12.6.0/local_installers/cuda_12.6.0_560.28.03_linux.run
 sh cuda_12.6.0_560.28.03_linux.run
+# CUDA Toolkit 12.6
+# change toolkit and lib path
+# /home/lixuan/cuda12.6
 
 wget https://developer.download.nvidia.com/compute/cudnn/redist/cudnn/linux-x86_64/cudnn-linux-x86_64-9.10.2.21_cuda12-archive.tar.xz
 tar -xvf cudnn-linux-x86_64-9.10.2.21_cuda12-archive.tar.xz
 
+cd cudnn-linux-x86_64-9.10.2.21_cuda12-archive || exit
 cp include/cudnn*.h /home/lixuan/cuda12.6/include/
 cp lib/libcudnn* /home/lixuan/cuda12.6/lib64/
 
--- sh/non-desktop/tools/server/zellij.sh
+++ sh/non-desktop/tools/server/zellij.sh
@@ -3,13 +3,13 @@
 
 cd ~/tools || exit
 
-curl -LO https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz
+curl -LO https://ghfast.top/https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz
 tar xf zellij-x86_64-unknown-linux-musl.tar.gz zellij
 cp zellij ~/.local/bin
 chmod +x ~/.local/bin/zellij
 
 mkdir -p ~/.local/share/zellij/plugins
 cd ~/.local/share/zellij/plugins || exit
-curl -LO https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm
-curl -LO https://github.com/h1romas4/zellij-datetime/releases/latest/download/zellij-datetime.wasm
+curl -LO https://ghfast.top/https://github.com/dj95/zjstatus/releases/latest/download/zjstatus.wasm
+curl -LO https://ghfast.top/https://github.com/h1romas4/zellij-datetime/releases/latest/download/zellij-datetime.wasm
 
