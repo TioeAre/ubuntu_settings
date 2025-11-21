c4a95ccaea613815bda66b04b51edf2589aeb3fa
TioeAre <are10032@outlook.com>
2025-11-21 20:21:46 +0800

    feat: add Neovim desktop entry, icon, and readest image; update .xsessionrc and filemanager script

--- sh/desktop/tools/filemanager.sh
+++ sh/desktop/tools/filemanager.sh
@@ -6,4 +6,5 @@ sudo apt install nautilus-actions filemanager-actions -y
 
 # https://github.com/bassmanitram/actions-for-nautilus
 # /usr/bin/x-terminal-emulator --working-directory=%d/%b
+# alacritty -o window.dimensions.columns=120 -o window.dimensions.lines=40 -e /home/tioeare/brew/bin/nvim %d/%b
 # nautilus -q
